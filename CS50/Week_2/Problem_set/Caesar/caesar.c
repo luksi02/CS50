00b9d41924beeb5eff2c866619805453fb227163
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-11-21 21:45:58 +0100

    6
    So far so good.

--- CS50/Week_2/Problem_set/Caesar/caesar.c
+++ CS50/Week_2/Problem_set/Caesar/caesar.c
@@ -41,19 +41,44 @@ int get_key(void)
     {
         n = get_int("Please insert key: ");
     } while (n < 1);
+    n = n % 26;
     return n;
 }
 
 char rotate(char c, int n)
 {
+    // int changed_value;
     // check for lowercase/uppercase/other characters in received string:
-    if (c >= 'a' && c <= 'z')
+    if (c >= 'a' && c <= 'z') // 'a' is 97 in ASCII, 'z' is 122
     {
+        // return printf("%c is lowercase \n", c);
+
+        // c = (int) c + (int) n;
+        // printf("%c int(c): %i \n", c, c);
+
+        if (c + n > 122) // when 'key' (represented by n here) reaches beyond letter 'z'
+        {
+            char new_c = 97 + (c + n - 122 - 1); // modulo from char c, but implemented here - '-1' to not omit first - 'a' letter
+            return printf("%c is lowercase, int(c): %i \n", new_c, new_c);
+        }
+        else
+        {
+            c = c + n;
+        }
         return printf("%c is lowercase \n", c);
     }
-    else if (c >= 'A' && c <= 'Z')
+    else if (c >= 'A' && c <= 'Z') // 'A' is 65 in ASCII, 'Z' is 90
     {
-        return printf("%c is uppercase \n", c);
+        if (c + n > 90) // when 'key' (represented by n here) reaches beyond letter 'z'
+        {
+            char new_c = 65 + (c + n - 90 - 1); // modulo from char c, but implemented here - '-1' to not omit first - 'a' letter
+            return printf("%c is uppercase, int(c): %i \n", new_c, new_c);
+        }
+        else
+        {
+            c = c + n;
+        }
+        return printf("Pre-coded: %c is uppercase \n", c);
     }
     else
     {
