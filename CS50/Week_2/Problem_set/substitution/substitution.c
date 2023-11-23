433378a17d6b740105e7d6a9ec0b32bfd997ae11
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-11-23 21:50:45 +0100

    8

--- CS50/Week_2/Problem_set/substitution/substitution.c
+++ CS50/Week_2/Problem_set/substitution/substitution.c
@@ -2,6 +2,7 @@
 // 26-character-long set of letters (as representation of alphabet), so message
 // is respresnted as their substitute in the key (as in alphabetical order)
 
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include "C:\\Users\\Luksi02\\Desktop\\CS50\\Week_2\\cs50.h"
@@ -9,43 +10,77 @@
 // example key: 'qwertyuiopasdfghjklzxcvbnm'
 
 string get_key(void);
-// char rotate(char c, int n);
+int isUniqueLetter(string input);
 
 int main(void)
 {
-    // Implement protection for doubling letters in code... but later xd
     string key = get_key();
     printf("Key: %s \n", key);
 
-    // Prompt user for plaintext
+    // string key = "qwertyuiopasdfghjklzxcvbnm";
+
     string plaintext = get_string("Insert text to encode: ");
     printf("Plaintext: %s \n", plaintext);
 
     int len = strlen(plaintext);
     printf("Length: %i \n", len);
 
-    // for (int i = 0; i < strlen(plaintext); i++)
     for (int i = 0; i < len; i++)
     {
-        // to simplify just lowercase for now - '97' stands for 'a' in ASCII
-        int index = plaintext[i] - 97;
-        printf("Plaintext char: %c code char: %c \n", plaintext[i], key[index]);
-        // rotate(plaintext[i], key);
+        if (plaintext[i] >= 'a' && plaintext[i] <= 'z')
+        {
+            int index = plaintext[i] - 97;
+            char coded_char = tolower(key[index]);
+            printf("Plaintext char: %c code char: %c \n", plaintext[i], coded_char);
+        }
+        else if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
+        {
+            int index = plaintext[i] - 65;
+            char coded_char = toupper(key[index]);
+            printf("Plaintext char: %c code char: %c \n", plaintext[i], coded_char);
+        }
+        else
+        {
+            printf("Plaintext char: %c", plaintext[i]);
+        }
     }
 }
-
-// TODO: 1 Protect code from recurring letters,
-// 2. translate only letters
-// 3. handle uppercase
 
 string get_key(void)
 {
     int n;
     string key;
+    int isUnique;
+
     do
     {
         key = get_string("Please insert 26-character key: ");
         n = strlen(key);
-    } while (n != 26);
+
+        isUnique = isUniqueLetter(key);
+
+    } while (n != 26 && !isUnique);
     return key;
+}
+
+int isUniqueLetter(string input)
+{
+    int charSet[26] = {};
+
+    for (int i = 0; i < strlen(input); i++)
+    {
+        char ch = tolower(input[i]);
+
+        if (isalpha(ch))
+        {
+            int charValue = ch - 'a';
+
+            if (charSet[charValue] > 0)
+            {
+                return 0;
+            }
+
+            charSet[charValue] = 1;
+        }
+    }
 }
